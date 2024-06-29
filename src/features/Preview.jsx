cd302a0458e9e37af91e3e16ef5f3e06668daab9
Pius <piusdonkor1@gmail.com>
2024-06-29 21:05:49 +0000

    Update Preview.jsx

--- src/features/Preview.jsx
+++ src/features/Preview.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+import { parseMarkdown } from "../utils/helper";
 
-export default function Preview() {
-  return <div>Preview</div>;
+const EditorSection = styled.div`
+  width: 50%;
+  padding: 20px;
+  overflow-y: auto;
+  height: calc(100vh - 60px); /* Adjust height based on navbar height */
+`;
+
+const PreviewSection = styled(EditorSection)`
+  border-left: 1px solid #ddd;
+`;
+export default function Preview({ input }) {
+  return <PreviewSection>{parseMarkdown(input)}</PreviewSection>;
 }

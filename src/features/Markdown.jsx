e623a8c2363a918c868163d03ef7c025734d424d
Pius <piusdonkor1@gmail.com>
2024-07-03 00:08:24 +0000

    Update Markdown.jsx

--- src/features/Markdown.jsx
+++ src/features/Markdown.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import ContentBar from "../ui/ContentBar";
+
 const EditorSection = styled.div`
   width: 50%;
-  padding: 20px;
   overflow-y: auto;
-  height: calc(100vh - 60px); /* Adjust height based on navbar height */
-`;
-const Editor = styled.textarea`
-  width: 100%;
-  height: calc(100% - 60px);
+  display: flex;
+  flex-direction: column;
+  min-height: calc(100vh - 60px); /* Adjust height based on navbar height */
 `;
 
-export default function Markdown({ input, setInput }) {
-  return (
+const Editor = styled.textarea`
+  width: 100%;
+  min-height: calc(99%-3rem);
+  border: transparent;
+  outline: none;
+  resize: none;
+  padding: 10px 20px;
+  font-size: var(--font-size-medium);
+  font-family: var(--roboto-mono);
+  line-height: var(--line-height);
+`;
+
+export default function Markdown({ input, setInput, fullPreview }) {
+  return fullPreview ? (
+    ""
+  ) : (
     <EditorSection>
+      <ContentBar>
+        <span>markdown</span>
+      </ContentBar>
       <Editor value={input} onChange={(e) => setInput(e.target.value)} />
     </EditorSection>
   );
